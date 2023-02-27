64fed69d99aa75be6c3fcbc043d462f712eb8e5d
ademalabas <ademalabas@gmail.com>
2023-02-27 22:31:52 +0300

    27.02.23

--- src/test/java/testData/JasonPlaceHolderTestData.java
+++ src/test/java/testData/JasonPlaceHolderTestData.java
@@ -6,11 +6,28 @@ public class JasonPlaceHolderTestData {
 
     public static HashMap<String,Object> expectedData;
 
-    public HashMap setUpDataTodos() {
+    public HashMap setUpDataTodos(){
 
+        /*
+         {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+}
+         */
+        expectedData = new HashMap<>();
 
+        expectedData.put("StatusCode",200);
+        expectedData.put("Server","cloudflare");
+        expectedData.put("userId",1);
+        expectedData.put("id",2.0);
+        expectedData.put("title","quis ut nam facilis et officia qui");
+        expectedData.put("completed",false);
 
+        return expectedData;
     }
+
 
 
 
