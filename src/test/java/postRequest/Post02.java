53b25fd605b94d427b4f9db27f86cb9cefb0bdeb
ademalabas <ademalabas@gmail.com>
2023-03-01 23:13:07 +0300

    01.03.23

---
+++ src/test/java/postRequest/Post02.java
@@ -0,0 +1,48 @@
+package postRequest;
+
+public class Post02 {
+    /*
+
+       Given
+           https://gorest.co.in/public/v2/users
+       And
+
+              Request Body olarak aşağıdaki pattern de body gönderiniz
+              {
+     "name": "Tenali Ramakrishna",
+     "gender": "male",
+     "email": "tenali.ramakrishna@15ce.com",
+     "status": "active"
+   }
+       When
+           Kullanıcı POST Methodu ile Request Gönderir
+       Then
+           Status Code un "201" olduğunu Assert et
+       And
+              Response Body nin aşağıdaki şekilde olduğunu doğrulayınız
+                {
+     "name": "Tenali Ramakrishna",
+     "gender": "male",
+     "email": "tenali.ramakrishna@15ce.com",
+     "status": "active"
+   }
+
+      */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/testData/GoRestApiTestData.java
@@ -0,0 +1,11 @@
+package testData;
+
+public class GoRestApiTestData {
+
+
+    //422
+    public int statusCodeForPostInvalid(){
+
+        return 422;
+    }
+}
