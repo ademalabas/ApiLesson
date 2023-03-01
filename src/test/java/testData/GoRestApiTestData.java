a76f512b6a9ed12b263382f581ed4460a17b88fe
ademalabas <ademalabas@gmail.com>
2023-03-02 00:01:00 +0300

    01.03.23

--- src/test/java/testData/GoRestApiTestData.java
+++ src/test/java/testData/GoRestApiTestData.java
@@ -12,4 +12,30 @@ public class GoRestApiTestData {
     }
 
 
+     /*
+     {
+  "name": "Tenali Ramakrishna",
+  "gender": "male",
+  "email": "tenali.ramakrishna@15ce.com",
+  "status": "active"
+}
+     */
+
+
+
+    public HashMap<String,String > reqBodyAndExpectedData(String name, String gender, String email, String status ){
+
+        HashMap<String ,String > reqBodyAndExpectedDataMap = new HashMap<>();
+
+        reqBodyAndExpectedDataMap.put("name",name);
+        reqBodyAndExpectedDataMap.put("gender",gender);
+        reqBodyAndExpectedDataMap.put("email",email);
+        reqBodyAndExpectedDataMap.put("status",status);
+
+
+        return reqBodyAndExpectedDataMap;
+    }
+
+
+
 }
