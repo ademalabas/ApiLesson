e3bf6ec0cbd0e5448291d492993a395b31343487
ademalabas <ademalabas@gmail.com>
2023-03-01 22:32:05 +0300

    01.03.23

--- src/test/java/getRequesr/Get11.java
+++ src/test/java/getRequesr/Get11.java
@@ -107,6 +107,9 @@ public class Get11 extends ApiZippopotamBaseURL {
         assertEquals(zippoPotamPojos.getCountryAbbreviation(),actualDataMap.get("country abbreviation"));
         assertEquals(zippoPotamPojos.getZippoPotamPlaces().getPlaceName(),(((Map)((List)actualDataMap.get("places")).get(0))).get("place name"));
 
+
+        assertEquals(zippoPotamPojos.getZippoPotamPlaces().getLatitude(),(((Map)((List)actualDataMap.get("places")).get(0)).get("latitude")));
+
     }
 
 
