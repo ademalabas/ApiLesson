f8b8ae3c1dc5001c066752587eed01f8ce661a8d
ademalabas <ademalabas@gmail.com>
2023-03-02 20:56:20 +0300

    02.03.23

---
+++ src/test/java/postRequest/Post03.java
@@ -0,0 +1,37 @@
+package postRequest;
+
+public class Post03 {
+
+ /*
+
+    Given
+        http://dummy.restapiexample.com/api/v1/create
+    And
+        Request Body olarak aşağıdaki pattern de body gönderiniz
+      {
+  "name": "Drake F.",
+  "salary": "40000",
+  "age": "27"
+}
+    When
+        Kullanıcı POST Methodu ile Request Gönderir
+    Then
+        Status Code un "200" olduğunu Assert et
+    And
+           Response Body nin aşağıdaki şekilde olduğunu doğrulayınız
+           {
+    "status": "success",
+    "data": {
+        "name": "Drake F.",
+        "salary": "40000",
+        "age": "27",
+        "id": 4545
+    },
+    "message": "Successfully! Record has been added."
+}
+
+   */
+
+
+
+}
