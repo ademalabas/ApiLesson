cd896d1ee214e8683c93b28b6d1da4a65aa12c5f
ademalabas <ademalabas@gmail.com>
2023-03-16 23:38:36 +0300

    16.03.23

--- src/test/java/trelloAPI/StepDefinitions.java
+++ src/test/java/trelloAPI/StepDefinitions.java
@@ -10,6 +10,10 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import testData.TrelloApiTestData;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import static io.restassured.RestAssured.given;
 
 public class StepDefinitions {
@@ -28,6 +32,8 @@ public class StepDefinitions {
     public static JsonPath jsonPath;
 
     public static String listID;
+
+    public static List<String> cardsID = new ArrayList<>();
 
     @Given("Kullanıcı Trello icin Base URL set eder {string}")
     public void kullanıcı_Trello_icin_Base_URL_set_eder(String baseURL) {
@@ -130,20 +136,89 @@ public class StepDefinitions {
     }
 
     @When("Kullanıcı card ccreate edebilmek icin ilgili url e POST metodu ile request atar {string},{string},{string},{string}")
-    public void kullanıcı_card_ccreate_edebilmek_icin_ilgili_url_e_POST_metodu_ile_request_atar(String string, String string2, String string3, String string4) {
-       
+    public void kullanıcı_card_ccreate_edebilmek_icin_ilgili_url_e_POST_metodu_ile_request_atar(String idPath, String id, String cardsPath, String cards) {
+
+        //#"https://api.trello.com/1/cards?idList=5abbe4b7ddc1b351ef961414&key=APIKey&token=APIToken"
+        specification.pathParams(idPath,id,
+                cardsPath,cards).
+                queryParams("idList",listID,
+                        "name","CwCARDS",
+                        "key", trelloApiTestData.getKey(),
+                        "token",trelloApiTestData.getToken());
+
+        String idPathFormated = String.format("{%s}",idPath);
+        String cardsPathFormated = String.format("{%s}",cardsPath);
+
+        response = given().
+                spec(specification).
+                contentType(ContentType.JSON).
+                when().
+                post("/" + idPathFormated + "/" + cardsPathFormated);
+
+
+        System.out.println("RESPONSE ");
+        response.prettyPrint();
+
     }
 
-    
+
 
     @When("Kullanıcı card is yi alir")
     public void kullanıcı_card_is_yi_alir() {
-        
+
+        jsonPath = response.jsonPath();
+        cardsID.add(jsonPath.getString("id"));
     }
     @Then("Kullanıcı vard create edildigini verify eder")
     public void kullanıcı_vard_create_edildigini_verify_eder() {
-       
+        response.
+                then().
+                assertThat().
+                statusCode(200);
     }
+
+    @When("Kullanıcı update islemi iicn ilgili url ePUT metodu ile request atar {string},{string},{string},{string}")
+    public void kullanıcı_update_islemi_iicn_ilgili_url_ePUT_metodu_ile_request_atar(String idPath, String id, String cardsPath, String cards) {
+
+        //https://api.trello.com/1/cards/{id}?key=APIKey&token=APIToken
+        Random random = new Random();
+        int numberOfRandom = random.nextInt(cardsID.size()); //Bu metot nextInt() in içerisne verileen bir paramternin 0 ile o değer arasında random bir sayı generate eder. [0,value)
+
+         specification.pathParams(idPath,id,
+                cardsPath,cards,
+                 "cardIdPath",cardsID.get(numberOfRandom)).
+                 queryParams("idList",listID,
+                         "name","Cw Card Update",
+                         "key",trelloApiTestData.getKey(),
+                         "token",trelloApiTestData.getToken());
+
+
+         String idPathFormated = String.format("{%s}",idPath);
+        String cardsPathFormated = String.format("{%s}",cardsPath);
+
+         response = given().
+                 spec(specification).
+                 contentType(ContentType.JSON).
+                 when().
+                 put("/" + idPathFormated + "/" + cardsPathFormated + "/{cardIdPath}");
+
+
+        System.out.println("RESPONSE ");
+        response.prettyPrint();
+
+    }
+
+
+
+
+    @Then("Kullanıcı basarili bir sekilde cardlardan birini update ettigini verify eder")
+    public void kullanıcı_basarili_bir_sekilde_cardlardan_birini_update_ettigini_verify_eder() {
+        response.
+                then().
+                assertThat().
+                statusCode(200);
+    }
+
 
 
 
